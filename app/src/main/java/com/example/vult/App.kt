fdc90fbe5f0049ec6f8dafa68b5b6ec22da1bef7
Lucas Liu <lucasliu7@hotmail.com>
2022-07-22 18:16:30 +1000

    have basic toolbar

--- app/src/main/java/com/example/vult/App.kt
+++ app/src/main/java/com/example/vult/App.kt
@@ -1,10 +1,14 @@
 package com.example.vult
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.example.vult.databinding.FragmentAppBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,16 +21,15 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class App : Fragment() {
+    private var _binding: FragmentAppBinding? = null
+    private val binding get() = _binding!!
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -34,7 +37,24 @@ class App : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_app, container, false)
+        _binding = FragmentAppBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.appToolbar.inflateMenu(R.menu.app_menu)
+        binding.appToolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.sync -> {
+                    true
+                }
+                R.id.settings -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/vult/MainActivity.kt
+++ app/src/main/java/com/example/vult/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.vult
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.core.view.MenuProvider
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
