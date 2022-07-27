7fa2acf768427010ff46056650692497fbb8d040
Lucas Liu <lucasliu7@hotmail.com>
2022-07-27 16:43:54 +1000

    enhanced search visuals

--- app/src/main/java/com/sqooid/vult/fragments/vault/Vault.kt
+++ app/src/main/java/com/sqooid/vult/fragments/vault/Vault.kt
@@ -4,27 +4,22 @@ import android.animation.ObjectAnimator
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.*
-import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.DecelerateInterpolator
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnticipateOvershootInterpolator
 import android.view.animation.OvershootInterpolator
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import androidx.core.animation.doOnEnd
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.sqooid.vult.database.Credential
-import com.sqooid.vult.database.CredentialField
-import com.sqooid.vult.database.CredentialRepository
 import com.sqooid.vult.databinding.FragmentVaultBinding
 import com.sqooid.vult.fragments.vault.recyclerview.MainAdapter
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import java.util.*
 
 class Vault : Fragment() {
     private var _binding: FragmentVaultBinding? = null
@@ -37,6 +32,9 @@ class Vault : Fragment() {
     private lateinit var viewModel: VaultViewModel
 
     private lateinit var adapter: MainAdapter
+
+    private var showSearchBar: Boolean? = null
+    private var searchBarOnCooldown: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -70,7 +68,7 @@ class Vault : Fragment() {
         }
 
         binding.swipeDownSync.setOnRefreshListener {
-            Log.d("app","swiped down to sync")
+            Log.d("app", "swiped down to sync")
             binding.swipeDownSync.isRefreshing = false
         }
 
@@ -79,22 +77,29 @@ class Vault : Fragment() {
         }
 
         binding.fabSearch.setOnClickListener {
-            ObjectAnimator.ofFloat(binding.searchBarWrapperCard, "translationY", binding.searchBarWrapperCard.height.toFloat() * 1.5f).apply {
-                duration = 200
-                interpolator = OvershootInterpolator()
-                start()
-                doOnEnd {
-                    binding.searchBar.requestFocus()
-                    val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.showSoftInput(binding.searchBar, InputMethodManager.SHOW_IMPLICIT)
-                }
+            showSearch(true)
+        }
+        binding.searchBar.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                hideSearch(true)
+                true
+            } else {
+                false
             }
         }
 
         binding.recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                if (dy != 0 || dx != 0)
-                closeSearch()
+                if (showSearchBar == null) {
+                    return
+                }
+                if ((dy != 0 || dx != 0) && binding.searchBar.isFocused && showSearchBar == true)
+                    hideSearch(true)
+                if (dy > 0 && showSearchBar == true) {
+                    hideSearch(false)
+                } else if (dy < 0 && showSearchBar == false) {
+                    showSearch(false)
+                }
             }
         })
 
@@ -116,10 +121,51 @@ class Vault : Fragment() {
         }
     }
 
-    private fun closeSearch() {
-        binding.searchBar.clearFocus()
-        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view?.windowToken, 0)
+    private fun createSearchBarAnimator(reverse: Boolean): ObjectAnimator {
+        return ObjectAnimator.ofFloat(
+            binding.searchBarWrapperCard,
+            "translationY",
+            binding.searchBarWrapperCard.height.toFloat() * if (reverse) -1.5f else 1.5f
+        ).apply {
+            duration = 200
+            interpolator = AnticipateOvershootInterpolator()
+        }
     }
 
+    private fun showSearch(focus: Boolean) {
+        if (searchBarOnCooldown && !focus) return
+        putSearchBarOnCooldown()
+        showSearchBar = true
+        createSearchBarAnimator(false).apply {
+            start()
+            if (focus)
+                doOnEnd {
+                    binding.searchBar.requestFocus()
+                    val imm =
+                        activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.showSoftInput(binding.searchBar, InputMethodManager.SHOW_IMPLICIT)
+                }
+        }
+    }
+
+    private fun hideSearch(unfocus: Boolean) {
+        if (searchBarOnCooldown) return
+        putSearchBarOnCooldown()
+        if (unfocus) {
+            binding.searchBar.clearFocus()
+            val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view?.windowToken, 0)
+        }
+        showSearchBar = false
+        createSearchBarAnimator(true).start()
+    }
+
+    private fun putSearchBarOnCooldown() {
+        searchBarOnCooldown = true
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                searchBarOnCooldown = false
+            }
+        }, 500)
+    }
 }
